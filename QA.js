54681dbdda2b1e6a07918a6437820dc67200e525
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-12-26 23:05:05 +0545

    hellow

---
+++ QA.js
@@ -0,0 +1,16 @@
+function submitForm() {
+  const name = document.getElementById("name").value;
+  const mood = document.querySelector('input[name="mood"]:checked').value;
+  const output = document.querySelector(".output");
+
+  if (name && mood) {
+    output.innerHTML = "Hello " + name + ". You feel " + mood + ".<br>";
+    if (mood == "fine") {
+      output.innerHTML += " Let's go for a trecking <br>";
+    } else {
+      output.innerHTML += " You will be good at all. <br>";
+    }
+  } else {
+    output.innerHTML = "Please answer all questions.";
+  }
+}
