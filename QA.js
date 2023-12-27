d4357f79936560982dc8fb960411fd1c233265c4
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-12-27 23:48:54 +0545

    added how are you

--- QA.js
+++ QA.js
@@ -2,11 +2,20 @@ function submitForm() {
   const name = document.getElementById("name").value;
   const mood = document.querySelector('input[name="mood"]:checked').value;
   const comment = document.getElementById("comment").value;
+  const Mood = document.getElementById("mood").value;
   const output = document.querySelector(".output");
-  
 
   if (name && mood) {
-    output.innerHTML = "Hello " + name + ". You feel " + mood + ".<br>" + comment +".<br>";
+    output.innerHTML =
+      "Hello " +
+      name +
+      ". You feel " +
+      mood +
+      ".." +
+      Mood +
+      ".<br>" +
+      comment +
+      ".<br>";
     if (mood == "fine") {
       output.innerHTML += " Let's go for a trecking <br>";
     } else {
