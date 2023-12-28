50a5973b963f2853dd010de4c862f1351721bf7d
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-12-28 07:31:16 +0545

    minimized output

--- QA.js
+++ QA.js
@@ -6,20 +6,10 @@ function submitForm() {
   const output = document.querySelector(".output");
 
   if (name && mood) {
-    output.innerHTML =
-      "Hello " +
-      name +
-      ". You feel " +
-      mood +
-      ".." +
-      Mood +
-      ".<br>" +
-      comment +
-      ".<br>";
     if (mood == "fine") {
-      output.innerHTML += " Let's go for a trecking <br>";
+      output.innerHTML += "Thank you " + name;
     } else {
-      output.innerHTML += " You will be good at all. <br>";
+      output.innerHTML += +name + " Hope good. <br>";
     }
   } else {
     output.innerHTML = "Please answer all questions.";
